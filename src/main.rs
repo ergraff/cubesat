7dc63d69940530befcc679d1140376c696d9658c
erik <erik@opensuse.localdomain>
2023-10-05 18:25:38 +0200

    Initial file structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod component;
+mod cubesat;
+mod orbit;
+mod time;
+mod vector;
+
 fn main() {
     println!("Hello, world!");
+    cubesat::foo();
+    vector::foo();
+    time::foo();
+    component::foo();
+    orbit::foo();
 }
