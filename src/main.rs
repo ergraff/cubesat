86a01725f9592ad3e53c08833f1a47aa0ac5ef39
ergraff <erikgraff39@gmail.com>
2023-12-17 01:58:58 +0100

    Adds multithreading
    Spawns thread for each simulation

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,22 @@ mod vector;
 
 use std::fs;
 use std::path::Path;
+use std::thread;
 
 fn main() {
     let path = Path::new("./input/");
     let files = fs::read_dir(path).expect("Unable to read input path!");
 
-    for file in files {
-        let f = file.expect("Unable to read file!").path();
-        println!("Simulating: {}", f.display());
+    let handle = thread::spawn(|| {
+        for file in files {
+            let f = file.expect("Unable to read file!").path();
+            let name = f.file_name().unwrap().to_str().unwrap();
+            println!("Simulating '{}'", name);
 
-        let mut cubesat = cubesat::CubeSat::from_toml(f.to_str().unwrap());
-        cubesat.simulate();
-    }
-    println!("Simulation ended");
+            let mut cubesat = cubesat::CubeSat::from_toml(f.to_str().unwrap());
+            cubesat.simulate();
+        }
+    });
+
+    handle.join().unwrap();
 }
